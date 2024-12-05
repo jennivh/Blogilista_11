71b43aab1c83845ac87c62eee9399744adb40745
jennivh <jvharakka@gmail.com>
2024-12-05 12:30:15 +0200

    port changed again

--- playwright.config.js
+++ playwright.config.js
@@ -1,22 +1,21 @@
 
 const { defineConfig, } = require('@playwright/test');
 
-const PORT = process.env.PORT || 5173;
 module.exports = defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
   
   use: {
-    baseURL: `http://localhost:${PORT}`
+    baseURL: `http://localhost:3001`
   },
 
   
 
   /* Run your local dev server before starting the tests */
    webServer: {
-    command: 'cd ./server && npm run dev',
-    url: `http://127.0.0.1:${PORT}`,
+    command: 'cd ./server && npm run start',
+    url: `http://127.0.0.1:3001`,
     reuseExistingServer: !process.env.CI,
   },
 });

b5d21908ac61cfd62d17ce11c5daee038e9fc89d
jennivh <jvharakka@gmail.com>
2024-12-05 22:55:19 +0200

    ..

--- client/end-to-end/playwright.config.js
+++ client/end-to-end/playwright.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from '@playwright/test'
 export default defineConfig({
   testDir: './tests',
   webServer: {
-    command: 'npm run dev',
+    command: 'cd .. && npm run dev',
     port: 5173,
     reuseExistingServer: !process.env.CI,
   },

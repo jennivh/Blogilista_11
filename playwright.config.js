c123db40d595496db62df4d4c405effa4ec53949
jennivh <jvharakka@gmail.com>
2024-12-05 15:30:22 +0200

    now?

--- playwright.config.js
+++ playwright.config.js
@@ -14,6 +14,7 @@ module.exports = defineConfig({
 
   /* Run your local dev server before starting the tests */
    webServer: {
+    command: 'cd ./server && npm start',
     url: `http://127.0.0.1:3001`,
     reuseExistingServer: !process.env.CI,
   },

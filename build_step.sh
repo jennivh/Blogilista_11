2a1dfc1e5c58f073fa297dac363c9ea11d1a11e4
jennivh <jvharakka@gmail.com>
2024-12-10 16:21:25 +0200

    deployment fixed?

--- build_step.sh
+++ build_step.sh
@@ -8,3 +8,5 @@ npm install
 cd ./client
 npm install
 npm run build
+cd ..
+npm run build
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+
 const path = require('path')
 const Person = require('./models/person')
 const cors = require('cors')
